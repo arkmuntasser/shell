2a7a2701ce3602c5d323bf4c2bdc6933bc0e8745
Amir R Muntasser <arkmuntasser@gmail.com>
2015-10-17 00:13:31 -0700

    improved load system

--- js/app/main.js
+++ js/app/main.js
@@ -1,35 +1,31 @@
 define(["jquery", "goatee"], function($, goatee) {
-  var headerContent = $("[data-header]");
-  var mainContent = $("[data-main]");
-  var footerContent = $("[data-footer]");
-  var headerFile = "templates/header/header.html";
-  var mainFile = "templates/main/one_col.html";
-  var footerFile = "templates/footer/footer.html";
+  var content = {
+    header : $("[data-header]"),
+    main : $("[data-main]"),
+    footer : $("[data-footer]")
+  };
 
-  $.get(headerFile, function(template) {
-    var data = {};
-    var html = goatee.fill(template, data);
+  var file = {
+    header : "templates/header/header.html",
+    main : "templates/main/one_col.html",
+    footer : "templates/footer/footer.html"
+  };
 
-    headerContent.html(html);
-  });
+  var load = function(section, filename) {
+    var file = "templates/main/" + filename + ".html";
 
-  $.get(mainFile, function(template) {
-    var data = {
-      title : "Hello, world!",
-      body : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    };
-    var html = goatee.fill(template, data);
+    $.get(file, function(template) {
+      var data = {
+        title : "Hello, world!",
+        body : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      };
+      var html = goatee.fill(template, data);
 
-    mainContent.html(html);
-  });
+      content[section].html(html);
+    });
+  };
 
-  $.get(footerFile, function(template) {
-    var data = {
-      title : "Hello, world!",
-      body : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    };
-    var html = goatee.fill(template, data);
-
-    footerContent.html(html);
-  });
+  load("header", "header");
+  load("main", "one_col");
+  load("footer", "footer");
 });
